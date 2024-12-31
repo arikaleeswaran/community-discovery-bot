b02926a3ccf55bc51831be68c30df82421f5f5f6
Arikaleeswaran G <115526282+arikaleeswaran@users.noreply.github.com>
2024-12-31 22:55:23 +0530

    Update event_service.py

--- app/services/event_service.py
+++ app/services/event_service.py
@@ -1,7 +1,7 @@
 import requests
 
 def fetch_events(location, interests):
-    api_key = 'YOUR_API_KEY'
+    api_key = 'API_KEY'
     url = f"https://api.eventbriteapi.com/v3/events/search/?location.address={location}&q={interests}&token={api_key}"
     response = requests.get(url)
     return response.json()['events']
